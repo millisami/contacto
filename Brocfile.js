cfe661c4e6bab92dbf8f3929b6def52a5bd12e14
Sachin <millisami@gmail.com>
2014-07-25 18:48:13 +0545

    Brocfile patched to load bootstrap glyphicons fonts

--- Brocfile.js
+++ Brocfile.js
@@ -17,4 +17,17 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-module.exports = app.toTree();
+app.import('vendor/ember_forms/dist/ember_forms.js');
+
+// Put the bootstrap fonts in the place that the bootstrap css expects to find them.
+var pickFiles = require('broccoli-static-compiler');
+var bootstrapFonts = pickFiles('vendor/bootstrap-sass-official/assets/fonts/bootstrap', {
+    srcDir: '/',
+    destDir: '/assets/bootstrap'
+});
+
+// Merge the bootstrapFonts with the ember app tree
+var mergeTrees = require('broccoli-merge-trees');
+module.exports = mergeTrees([app.toTree(),bootstrapFonts]);
+
+// module.exports = app.toTree();
